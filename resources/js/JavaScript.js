24ef1ac2bb811f44f6ce5e364bb61c5b05e39231
Álvaro Masedo <alvaromasedoperez@gmail.com>
2025-10-29 09:32:21 +0100

    Created all the HTML and CSS. Updated the index.html

--- resources/js/JavaScript.js
+++
@@ -1,5 +0,0 @@
-const blade = document.querySelector(".lightsaber .blade");
-
-document.querySelector(".switchbutton").addEventListener("click", function(){
-    blade.classList.toggle("blade-height");
-});
